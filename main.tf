86b5b60a6f73b6c107320a8eceb6c90aad546560
Paul Knopf <paul.knopf@ensemblehp.com>
2024-12-29 01:40:49 -0500

    wip

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@
 # Local declarations
 #---------------------------------
 locals {
-  resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rg.*.name, [""]), 0)
-  location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)
   public_ip_map       = { for pip in var.public_ip_names : pip => true }
 
   fw_nat_rules = { for idx, rule in var.firewall_nat_rules : rule.name => {
@@ -26,32 +24,11 @@ locals {
 }
 
 #---------------------------------------------------------
-# Resource Group Creation or selection - Default is "true"
-#----------------------------------------------------------
-data "azurerm_resource_group" "rgrp" {
-  count = var.create_resource_group == false ? 1 : 0
-  name  = var.resource_group_name
-}
-
-resource "azurerm_resource_group" "rg" {
-  count    = var.create_resource_group ? 1 : 0
-  name     = lower(var.resource_group_name)
-  location = var.location
-  tags     = merge({ "ResourceName" = format("%s", var.resource_group_name) }, var.tags, )
-}
-
-data "azurerm_storage_account" "storeacc" {
-  count               = var.storage_account_name != null ? 1 : 0
-  name                = var.storage_account_name
-  resource_group_name = local.resource_group_name
-}
-
-#---------------------------------------------------------
 # Firewall Subnet Creation or selection
 #----------------------------------------------------------
 resource "azurerm_subnet" "fw-snet" {
   name                 = "AzureFirewallSubnet"
-  resource_group_name  = local.resource_group_name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
   address_prefixes     = var.firewall_subnet_address_prefix #[cidrsubnet(element(var.vnet_address_space, 0), 10, 0)]
   service_endpoints    = var.firewall_service_endpoints
@@ -63,7 +40,7 @@ resource "azurerm_subnet" "fw-snet" {
 resource "azurerm_subnet" "fw-mgnt-snet" {
   count                = var.enable_forced_tunneling ? 1 : 0
   name                 = "AzureFirewallManagementSubnet"
-  resource_group_name  = local.resource_group_name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
   address_prefixes     = var.firewall_management_subnet_address_prefix #[cidrsubnet(element(var.vnet_address_space, 0), 10, 0)]
 }
@@ -73,8 +50,8 @@ resource "azurerm_subnet" "fw-mgnt-snet" {
 #------------------------------------------
 resource "azurerm_public_ip_prefix" "fw-pref" {
   name                = lower("${var.firewall_config.name}-pip-prefix")
-  resource_group_name = local.resource_group_name
-  location            = local.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
   prefix_length       = var.public_ip_prefix_length
   tags                = merge({ "ResourceName" = lower("${var.firewall_config.name}-pip-prefix") }, var.tags, )
 }
@@ -82,8 +59,8 @@ resource "azurerm_public_ip_prefix" "fw-pref" {
 resource "azurerm_public_ip" "fw-pip" {
   for_each            = local.public_ip_map
   name                = lower("pip-${var.firewall_config.name}-${each.key}")
-  location            = local.location
-  resource_group_name = local.resource_group_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   allocation_method   = "Static"
   sku                 = "Standard"
   public_ip_prefix_id = azurerm_public_ip_prefix.fw-pref.id
@@ -93,8 +70,8 @@ resource "azurerm_public_ip" "fw-pip" {
 resource "azurerm_public_ip" "fw-mgnt-pip" {
   count               = var.enable_forced_tunneling ? 1 : 0
   name                = lower("pip-${var.firewall_config.name}-fw-mgnt")
-  location            = local.location
-  resource_group_name = local.resource_group_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   allocation_method   = "Static"
   sku                 = "Standard"
   tags                = merge({ "ResourceName" = lower("pip-${var.firewall_config.name}-fw-mgnt") }, var.tags, )
@@ -105,8 +82,8 @@ resource "azurerm_public_ip" "fw-mgnt-pip" {
 #-----------------
 resource "azurerm_firewall" "fw" {
   name                = format("%s", var.firewall_config.name)
-  resource_group_name = local.resource_group_name
-  location            = local.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
   sku_name            = var.firewall_config.sku_name
   sku_tier            = var.firewall_config.sku_tier
   # firewall_policy_id  = var.firewall_policy != null ? azurerm_firewall_policy.fw-policy.0.id : null
@@ -150,9 +127,9 @@ resource "azurerm_firewall" "fw" {
 #----------------------------------------------
 resource "azurerm_firewall_application_rule_collection" "fw_app" {
   for_each            = local.fw_application_rules
-  name                = lower(format("fw-app-rule-%s-${var.firewall_config.name}-${local.location}", each.key))
+  name                = lower(format("fw-app-rule-%s-${var.firewall_config.name}-${var.location}", each.key))
   azure_firewall_name = azurerm_firewall.fw.name
-  resource_group_name = local.resource_group_name
+  resource_group_name = var.resource_group_name
   priority            = 100 * (each.value.idx + 1)
   action              = each.value.rule.action
 
@@ -174,9 +151,9 @@ resource "azurerm_firewall_application_rule_collection" "fw_app" {
 
 resource "azurerm_firewall_network_rule_collection" "fw" {
   for_each            = local.fw_network_rules
-  name                = lower(format("fw-net-rule-%s-${var.firewall_config.name}-${local.location}", each.key))
+  name                = lower(format("fw-net-rule-%s-${var.firewall_config.name}-${var.location}", each.key))
   azure_firewall_name = azurerm_firewall.fw.name
-  resource_group_name = local.resource_group_name
+  resource_group_name = var.resource_group_name
   priority            = 100 * (each.value.idx + 1)
   action              = each.value.rule.action
 
@@ -193,9 +170,9 @@ resource "azurerm_firewall_network_rule_collection" "fw" {
 
 resource "azurerm_firewall_nat_rule_collection" "fw" {
   for_each            = local.fw_nat_rules
-  name                = lower(format("fw-nat-rule-%s-${var.firewall_config.name}-${local.location}", each.key))
+  name                = lower(format("fw-nat-rule-%s-${var.firewall_config.name}-${var.location}", each.key))
   azure_firewall_name = azurerm_firewall.fw.name
-  resource_group_name = local.resource_group_name
+  resource_group_name = var.resource_group_name
   priority            = 100 * (each.value.idx + 1)
   action              = each.value.rule.action
 
@@ -244,58 +221,57 @@ resource "azurerm_firewall_policy" "fw-policy" {
 # azurerm monitoring diagnostics - Firewall and Public IP's
 #---------------------------------------------------------------
 resource "azurerm_monitor_diagnostic_setting" "fw-mgnt-pip-diag" {
-  count                      = var.log_analytics_workspace_id != null || var.storage_account_name != null && var.enable_forced_tunneling ? 1 : 0
+  count                      = var.enable_forced_tunneling ? 1 : 0
   name                       = lower("fw-${var.firewall_config.name}-mgnt-pip-diag")
   target_resource_id         = azurerm_public_ip.fw-mgnt-pip.0.id
-  storage_account_id         = var.storage_account_name != null ? data.azurerm_storage_account.storeacc.0.id : null
+  storage_account_id         = var.storage_account_id
   log_analytics_workspace_id = var.log_analytics_workspace_id
 
-  dynamic "log" {
-    for_each = var.fw_pip_diag_logs
-    content {
-      category = log.value
-      enabled  = true
+  # dynamic "log" {
+  #   for_each = var.fw_pip_diag_logs
+  #   content {
+  #     category = log.value
+  #     enabled  = true
 
-      retention_policy {
-        enabled = false
-        days    = 0
-      }
-    }
-  }
+  #     retention_policy {
+  #       enabled = false
+  #       days    = 0
+  #     }
+  #   }
+  # }
 
   metric {
     category = "AllMetrics"
 
-    retention_policy {
-      enabled = false
-      days    = 0
-    }
-  }
-
-  lifecycle {
-    ignore_changes = [log, metric]
+    # `retention_policy` has been deprecated in favor of
+    # │ `azurerm_storage_management_policy` resource - to learn more
+    # │ https://aka.ms/diagnostic_settings_log_retention
+    # retention_policy {
+    #   enabled = false
+    #   days    = 0
+    # }
   }
 }
 
 resource "azurerm_monitor_diagnostic_setting" "fw-pip-diag" {
-  for_each                   = { for pip in var.public_ip_names : pip => true if var.log_analytics_workspace_id != null || var.storage_account_name != null }
+  for_each                   = toset(var.public_ip_names)
   name                       = lower("fw-${var.firewall_config.name}-${each.key}-pip-diag")
   target_resource_id         = azurerm_public_ip.fw-pip[each.key].id
-  storage_account_id         = var.storage_account_name != null ? data.azurerm_storage_account.storeacc.0.id : null
+  storage_account_id         = var.storage_account_id
   log_analytics_workspace_id = var.log_analytics_workspace_id
 
-  dynamic "log" {
-    for_each = var.fw_pip_diag_logs
-    content {
-      category = log.value
-      enabled  = true
+  # dynamic "log" {
+  #   for_each = var.fw_pip_diag_logs
+  #   content {
+  #     category = log.value
+  #     enabled  = true
 
-      retention_policy {
-        enabled = false
-        days    = 0
-      }
-    }
-  }
+  #     retention_policy {
+  #       enabled = false
+  #       days    = 0
+  #     }
+  #   }
+  # }
 
   metric {
     category = "AllMetrics"
@@ -304,32 +280,27 @@ resource "azurerm_monitor_diagnostic_setting" "fw-pip-diag" {
       enabled = false
       days    = 0
     }
-  }
-
-  lifecycle {
-    ignore_changes = [log, metric]
   }
 }
 
 resource "azurerm_monitor_diagnostic_setting" "fw-diag" {
-  count                      = var.log_analytics_workspace_id != null || var.storage_account_name != null ? 1 : 0
   name                       = lower("${var.firewall_config.name}-diag")
   target_resource_id         = azurerm_firewall.fw.id
-  storage_account_id         = var.storage_account_name != null ? data.azurerm_storage_account.storeacc.0.id : null
+  storage_account_id         = var.storage_account_id
   log_analytics_workspace_id = var.log_analytics_workspace_id
 
-  dynamic "log" {
-    for_each = var.fw_diag_logs
-    content {
-      category = log.value
-      enabled  = true
+  # dynamic "log" {
+  #   for_each = var.fw_diag_logs
+  #   content {
+  #     category = log.value
+  #     enabled  = true
 
-      retention_policy {
-        enabled = false
-        days    = 0
-      }
-    }
-  }
+  #     retention_policy {
+  #       enabled = false
+  #       days    = 0
+  #     }
+  #   }
+  # }
 
   metric {
     category = "AllMetrics"
@@ -338,9 +309,5 @@ resource "azurerm_monitor_diagnostic_setting" "fw-diag" {
       enabled = false
       days    = 0
     }
-  }
-
-  lifecycle {
-    ignore_changes = [log, metric]
   }
 }
--- variables.tf
+++ variables.tf
@@ -161,12 +161,10 @@ variable "fw_diag_logs" {
 
 variable "log_analytics_workspace_id" {
   description = "The resource id of log analytics workspace"
-  default     = null
 }
 
-variable "storage_account_name" {
-  description = "The name of the hub storage account to store logs"
-  default     = null
+variable "storage_account_id" {
+  description = "The id of the hub storage account to store logs"
 }
 
 variable "tags" {
